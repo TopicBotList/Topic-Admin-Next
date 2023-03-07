cd0667c4a86e9a2fe30b46a9b7b880dc7b66df79
Miya <foenx0315@gmail.com>
2023-03-07 15:20:33 +0530

    i have to say i am dumb.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Bar from "./CLUI";
-import { FaHome, FaRobot, FaServer, FaSignOutAlt, FaUser } from 'react-icons/fa'
+import { FaHome, FaRobot, FaServer, FaHandshake, FaSignOutAlt, FaUser } from 'react-icons/fa'
 import SidebarItem from "./SidebarItem";
 import { useRouter } from "next/router";
 export default function Navbar({children}){
@@ -20,7 +20,7 @@ export default function Navbar({children}){
             <SidebarItem onClick={()=> router.push('/bots')} text={'Bots'} icon={<FaRobot/>}/>
             <SidebarItem onClick={()=> router.push('/servers')} text={'Servers'} icon={<FaServer/>}/>
             <SidebarItem onClick={()=> router.push('/staff')} text={'Staff'} icon={<FaUser/>}/>
-            <SidebarItem onClick={()=> router.push('/partner')} text={'Partners'}/>
+            <SidebarItem onClick={()=> router.push('/partner')} text={'Partners'} icon={<FaHandshake/>}/>
 
         </div>
     </div>
--- pages/partner.js
+++ pages/partner.js
@@ -1,4 +1,4 @@
-import { FaCrown, FaPaintBrush, FaPaintRoller, FaUser, FaUserCog } from "react-icons/fa";
+import { FaCrown, FaHandshake, FaPaintBrush, FaPaintRoller, FaUser, FaUserCog } from "react-icons/fa";
 
 export default function Staff(){
 return(<>
@@ -6,10 +6,12 @@ return(<>
         <div className="flex items-center bg-blue-900/10 p-3">
             <img src={'https://cdn.topiclist.xyz/images/png/TopicList5.png'} className={'h-[150px] w-[150px] rounded-lg'}></img>
             <div className="flex flex-col h-[130px] ml-3">
-                <p className="font-bold text-4xl"></p>
-                <p className="font-semibold text-lg text-white/70"></p>
+                <p className="font-bold text-4xl">Partner name</p>
+                <p className="font-semibold text-lg text-white/70">Partner's detail</p>
                 <div className={'h-full'}/>
                 <div className="flex items-center">
+                    <FaHandshake className="text-3xl text-yellow-400/70"/>
+
                 </div>
             </div>
             
