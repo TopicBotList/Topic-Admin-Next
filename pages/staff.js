290eb2551c2806d4411568e7abaa7363dbc42554
Miya <foenx0315@gmail.com>
2023-03-07 14:12:13 +0530

    new staff

--- pages/staff.js
+++ pages/staff.js
@@ -20,10 +20,13 @@ export default function Staff(){
     </div>
     <div className="w-full flex flex-col py-2">
         <div className="flex items-center bg-blue-900/10 p-3">
-            <img src={'https://cdn.topiclist.xyz/images/png/TopicList5.png'} className={'h-[150px] w-[150px] rounded-lg'}></img>
+            <img src={'https://cdn.discordapp.com/avatars/510065483693817867/a_b27090c7931c8e28d91683fe102668cc.gif?size=1024'} className={'h-[150px] w-[150px] rounded-lg'}></img>
             <div className="flex flex-col h-[130px] ml-3">
-                <p className="font-bold text-4xl">Username</p>
-                <p className="font-semibold text-lg text-white/70">Short bio</p>
+                <p className="font-bold text-4xl">Toxic Dev</p>
+                <p className="font-semibold text-lg text-white/70">Canadian | Fullstack Software Dev | He/Him
+
+- Elixir - 💍 12-30-20 | ❤️ 03-22-19
+• https://linkcord.bio/TheRealToxicDev</p>
                 <div className={'h-full'}/>
                 <div className="flex items-center">
                     <FaCrown className="text-3xl text-yellow-400/70"/>
