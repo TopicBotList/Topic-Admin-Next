bdcb6b4c570b8d7466151b700a756766510b6348
Miya <foenx0315@gmail.com>
2023-03-24 11:21:58 +0530

    mmh

--- pages/bots.js
+++ pages/bots.js
@@ -1,4 +1,4 @@
-import { FaCrown, FaHandshake, FaPaintBrush, IoBanSharp, FaPaintRoller, FaUser, FaUserCog, FaHammer, MdOutlinePersonRemove } from "react-icons/fa";
+import { FaHammer, MdOutlinePersonRemove } from "react-icons/fa";
 
 export default function Staff(){
   return(
