29c6e3e4802aff12871d9566c4f6e8b5be41a5c4
Miya <foenx0315@gmail.com>
2023-03-24 11:11:39 +0530

    removed the fetch function because why not

--- pages/bots.js
+++ pages/bots.js
@@ -1,37 +1,25 @@
-import { useState, useEffect } from "react";
-import { FaCrown, FaHandshake, FaPaintBrush, FaHammer, MdOutlinePersonRemove } from "react-icons/fa";
+import { FaCrown, FaHandshake, FaPaintBrush, IoBanSharp, FaPaintRoller, FaUser, FaUserCog, FaHammer, MdOutlinePersonRemove } from "react-icons/fa";
 
-export default function Staff() {
-  const [botInfo, setBotInfo] = useState(null);
-
-  useEffect(() => {
-    fetch("https://adminapi.topiclist.xyz/bot/info")
-      .then((response) => response.json())
-      .then((data) => setBotInfo(data));
-  }, []);
-
-  if (!botInfo) {
-    return <p>Loading...</p>;
-  }
-
-  return (
+export default function Staff(){
+  return(
     <>
       <div className="w-full flex flex-col py-2">
         <div className="flex items-center bg-blue-900/10 p-3">
-          <img src={`https://cdn.discordapp.com/avatars/${botInfo.id}/${botInfo.avatar}.png`} className={'h-[150px] w-[150px] rounded-lg'}></img>
+          <img src={'https://cdn.topiclist.xyz/images/png/TopicList5.png'} className={'h-[150px] w-[150px] rounded-lg'}></img>
           <div className="flex flex-col h-[130px] ml-3">
-            <p className="font-bold text-4xl">{botInfo.username}#{botInfo.discriminator}</p>
-            <p className="font-semibold text-lg text-white/70">{botInfo.bio}</p>
-            <div className={'h-full'}/>
             <div className="flex items-center">
-              <FaHandshake className="text-3xl text-yellow-400/70" />
-              <button className="ml-2 p-2 rounded-md bg-red-600 text-white hover:bg-red-700 focus:outline-none">
-                <FaHammer className="mr-2" />
-                Ban
+              <img src="https://cdn.discordapp.com/avatars/1234567890/abcdef1234567890abcdef1234567890.png?size=128" alt="Bot avatar" className="w-12 h-12 rounded-full"/>
+              <h1 className="text-4xl font-bold ml-4">Bot username#1234</h1>
+            </div>
+            <p className="font-semibold text-lg text-white/70">Discord bot description goes here</p>
+            <div className="h-full"></div>
+            <div className="flex items-center space-x-4">
+              <FaHandshake className="text-3xl text-yellow-400/70"/>
+              <button className="flex items-center bg-red-600 px-2 py-1 rounded-lg text-white hover:bg-red-700">
+                <FaHammer className="mr-2"/> Ban
               </button>
-              <button className="ml-2 p-2 rounded-md bg-red-600 text-white hover:bg-red-700 focus:outline-none">
-                <MdOutlinePersonRemove className="mr-2" />
-                Remove
+              <button className="flex items-center bg-yellow-600 px-2 py-1 rounded-lg text-white hover:bg-yellow-700">
+                <MdOutlinePersonRemove className="mr-2"/> Remove
               </button>
             </div>
           </div>
