5449d5307bba6f859721af88e4e97860e46a696a
Miya <foenx0315@gmail.com>
2023-03-25 18:09:53 +0530

    hmm

--- pages/partner.js
+++ pages/partner.js
@@ -1,22 +1,27 @@
-import {  FaHandshake } from "react-icons/fa";
+import { FaHandshake } from "react-icons/fa";
 
-export default function Staff(){
-  return(
+export default function Staff() {
+  return (
     <>
       <div className="w-full flex flex-col py-2 relative">
         <div className="flex items-center bg-blue-900/10 p-3">
-          <img src={'https://beta.diswidgets.org/ibl.png'} className={'h-[150px] w-[150px] rounded-lg'} />
+          <img
+            src={"https://beta.diswidgets.org/ibl.png"}
+            className={"h-[150px] w-[150px] rounded-lg"}
+          />
           <div className="flex flex-col h-[130px] ml-3">
             <p className="font-bold text-4xl">InfinityBots</p>
-            <p className="font-semibold text-lg text-white/70">We make it easier for you to advertise and grow your Discord Bots using our bot packs, vanity link's, stylish widget's and more.</p>
+            <p className="font-semibold text-lg text-white/70">
+              We make it easier for you to advertise and grow your Discord Bots using our bot packs, vanity link's, stylish widget's and more.
+            </p>
             <div className="h-full" />
             <div className="flex items-center">
               <FaHandshake className="text-3xl text-yellow-400/70" />
             </div>
           </div>
         </div>
-        <div className="absolute top-0 right-0 bg-white/50 px-2 py-1 rounded-full">
-          <p className="text-sm text-gray-700">Toxic Dev#5936</p>
+        <div className="absolute top-2 right-0 bg-white/50 px-2 py-1 rounded-full mt-2">
+          <p className="text-sm text-blue-700">Toxic Dev#5936</p>
         </div>
       </div>
     </>
