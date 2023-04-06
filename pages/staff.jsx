1728d6464e883c08160a9e536c24572658bc26d4
Miya <foenx0315@gmail.com>
2023-04-06 22:00:43 +0530

    hmmmmmm

--- pages/staff.jsx
+++ pages/staff.jsx
@@ -4,7 +4,7 @@ export default function Staff(){
     return(<>
     <div className="w-full flex flex-col py-2">
         <div className="flex items-center bg-blue-900/10 p-3">
-            <img src={'https://cdn.discordapp.com/avatars/787241442770419722/d57d6439f795238f3f58981ac4ab055c.png?size=1024'} className={'h-[150px] w-[150px] rounded-lg'}></img>
+            <img src={'https://cdn.discordapp.com/avatars/787241442770419722/d393ffaa50a963efd137f1032028678f.png?size=1024'} className={'h-[150px] w-[150px] rounded-lg'}></img>
             <div className="flex flex-col h-[130px] ml-3">
                 <p className="font-bold text-4xl">RanveerSoni</p>
                 <p className="font-semibold text-lg text-white/70">Creator Of This Site And BotList</p>
