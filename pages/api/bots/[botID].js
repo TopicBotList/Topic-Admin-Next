bed036a2818a65fdfec20bb766826506ceed97f8
Miya <foenx0315@gmail.com>
2023-04-28 17:25:27 +0530

    got the thingy's

--- pages/api/bots/[botID].js
+++ pages/api/bots/[botID].js
@@ -1,20 +1,6 @@
-// Import the necessary modules
-import fetch from 'node-fetch';
-
-// Define the handler function for the API endpoint
-async function handler(req, res) {
-  // Extract the botID parameter from the request query
+export default async function handler(req, res) {
   const { botID } = req.query;
-  
-  // Make a request to the external API to fetch data for the given botID
   const response = await fetch(`https://api.topiclist.xyz/bots/${botID}`);
-  
-  // Parse the response data as JSON
   const data = await response.json();
-  
-  // Send the JSON response back to the client
   res.status(200).json(data);
-}
-
-// Export the handler function as the default module export
-module.exports = handler;
+};
