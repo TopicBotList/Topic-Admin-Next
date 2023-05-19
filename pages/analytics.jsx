b3e84a790add8172110f4ee6dff5f028a797b510
Maya <foenx0315@gmail.com>
2023-05-19 19:45:36 +0530

    uhh holographic cube

--- pages/analytics.jsx
+++ pages/analytics.jsx
@@ -1,31 +1,13 @@
-import { useRouter } from "next/router";
+import React from 'react';
+import HologramCube from '../components/HologramCube';
 
-export default function ErrorPage() {
-  const router = useRouter();
+const HomePage = () => {
   return (
-    <div className=" bg-transparent h-full w-full flex flex-col justify-center items-center">
-      <h1 className="text-white text-6xl font-bold">404</h1>
-      <p className="text-white/70 text-lg font-semibold mt-3">
-        This page might have been moved, deleted, or never existed.
-      </p>
-      <div className="flex mt-6 items-center">
-        <button
-          onClick={() => {
-            router.replace("/");
-          }}
-          className="rounded-lg text-white bg-blue-600 py-3 px-4 min-w-[100px]"
-        >
-          Home
-        </button>
-        <button
-          onClick={() => {
-            router.back();
-          }}
-          className="rounded-lg text-white bg-zinc-900 py-3 px-4 min-w-[100px] ml-3 "
-        >
-          Back
-        </button>
-      </div>
+    <div>
+      <h1>Hologram Cube</h1>
+      <HologramCube />
     </div>
   );
-}
+};
+
+export default amalytics;
